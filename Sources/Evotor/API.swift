ff132526c2307706d30ce41d614694d0c286c4cf
Vitaly Berg <bergusman@gmail.com>
2018-08-21 01:15:55 +0300

    Add document endpoint to api and make api refactoring

--- Sources/Evotor/API.swift
+++ Sources/Evotor/API.swift
@@ -4,11 +4,29 @@ public final class API {
     public let token: String
     public init(token: String) {
         self.token = token
+        
+        iso8601 = DateFormatter()
+        iso8601.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZ"
+        
+        decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .formatted(iso8601)
+    }
+    
+    private let iso8601: DateFormatter
+    private let decoder: JSONDecoder
+    
+    public enum Error: Swift.Error {
+        case parsing(Swift.Error)
+        case noResponse
+        case noData
+        case notFound
+        case unauthorized
+        case badStatus(Int)
     }
     
     public enum Result<T> {
         case success(T)
-        case failure(Error)
+        case failure(Swift.Error)
     }
     
     public typealias Completion<T> = (Result<T>) -> Void
@@ -25,6 +43,36 @@ public final class API {
         get("/inventories/devices/search", completion: completion)
     }
     
+    public func products(of store: String, completion: @escaping Completion<[Product]>) {
+        get("/inventories/stores/\(store)/products", completion: completion)
+    }
+    
+    public func documents(
+        of store: String,
+        on device: String? = nil,
+        after: Date? = nil,
+        before: Date? = nil,
+        types: [DocumentType] = [],
+        completion: @escaping Completion<[Document]>
+        ) {
+        var params: [String: String] = [:]
+        
+        if let device = device {
+            params["deviceUuid"] = device
+        }
+        if let after = after {
+            params["gtCloseDate"] = iso8601.string(from: after)
+        }
+        if let before = before {
+            params["ltCloseDate"] = iso8601.string(from: before)
+        }
+        if types.count > 0 {
+            params["types"] = types.map({ $0.rawValue }).joined(separator: ",")
+        }
+        
+        get("/inventories/stores/\(store)/documents", params: params, completion: completion)
+    }
+    
     private func get<T: Decodable>(_ path: String, params: [String: String] = [:], completion: @escaping Completion<T>) {
         var comps = URLComponents(string: "https://api.evotor.ru/api/v1\(path)")!
         comps.queryItems = params.reduce(into: [], { $0.append(URLQueryItem(name: $1.key, value: $1.value)) })
@@ -37,15 +85,33 @@ public final class API {
                 completion(.failure(error))
                 return
             }
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(Error.noResponse))
+                return
+            }
             
-            if let data = data {
-                let decoder = JSONDecoder()
-                do {
-                    let value = try decoder.decode(T.self, from: data)
-                    completion(.success(value))
-                } catch {
-                    completion(.failure(error))
+            guard 200 ..< 300 ~= response.statusCode else {
+                switch response.statusCode {
+                case 401:
+                    completion(.failure(Error.unauthorized))
+                case 404:
+                    completion(.failure(Error.notFound))
+                default:
+                    completion(.failure(Error.badStatus(response.statusCode)))
                 }
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(Error.noData))
+                return
+            }
+            
+            do {
+                let value = try self.decoder.decode(T.self, from: data)
+                completion(.success(value))
+            } catch {
+                completion(.failure(Error.parsing(error)))
             }
         }
         task.resume()
